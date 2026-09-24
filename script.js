f8b5b8e774188d972c8c5c18c3571af329849381
chrisnsalda <sylvain.chrisnalda@escola.pr.gov.br>
2026-09-23 22:24:19 -0300

    jg

--- script.js
+++ script.js
@@ -1,61 +1,20 @@
-// MENU MOBILE
+function addToCart(productId) {
+    const product = products.find(
+        item => item.id === productId
+    );
 
-const menuButton = document.getElementById("menuButton");
-const menu = document.getElementById("menu");
+    const existingProduct = cart.find(
+        item => item.id === productId
+    );
 
-menuButton.addEventListener("click", function () {
-    menu.classList.toggle("active");
-});
-
-
-// MODO ESCURO
-
-const themeButton = document.getElementById("themeButton");
-
-themeButton.addEventListener("click", function () {
-
-    document.body.classList.toggle("dark");
-
-    if (document.body.classList.contains("dark")) {
-        themeButton.textContent = "☀️";
+    if (existingProduct) {
+        existingProduct.quantity++;
     } else {
-        themeButton.textContent = "🌙";
+        cart.push({
+            ...product,
+            quantity: 1
+        });
     }
 
-});
-
-
-// FORMULÁRIO
-
-const form = document.getElementById("contactForm");
-const formMessage = document.getElementById("formMessage");
-
-form.addEventListener("submit", function (event) {
-
-    event.preventDefault();
-
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    if (
-        name.trim() === "" ||
-        email.trim() === "" ||
-        message.trim() === ""
-    ) {
-        formMessage.textContent =
-            "Preencha todos os campos.";
-
-        formMessage.style.color = "red";
-
-        return;
-    }
-
-    formMessage.textContent =
-        `Obrigado, ${name}! Sua mensagem foi enviada com sucesso.`;
-
-    formMessage.style.color = "green";
-
-    form.reset();
-
-});
+    renderCart();
+}
